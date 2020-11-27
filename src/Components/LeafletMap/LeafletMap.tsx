a397d46574f37ea2bff4b083fb095b217ff223eb
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-11-27 20:11:00 +0100

    Basic React-leaflet map control and coordinates collecting.

--- src/Components/LeafletMap/LeafletMap.tsx
+++ src/Components/LeafletMap/LeafletMap.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import {Icon, IconOptions, LatLngTuple} from "leaflet";
+import {Icon, IconOptions, LatLng, LatLngTuple, LeafletEvent, LeafletMouseEvent} from "leaflet";
 import './LeafletMap.css';
 
 export interface LeafletMapPropsInterface {
@@ -8,22 +8,56 @@ export interface LeafletMapPropsInterface {
 }
 
 export interface LeafletMapStateInterface {
-
+    position: LatLngTuple
 }
 
 export default class LeafletMap extends React.Component<LeafletMapPropsInterface, LeafletMapStateInterface> {
 
+    iconOptions: IconOptions;
+    icon: Icon;
+    zoom: number;
+
+    /**
+     * @param props
+     */
+    constructor(props: LeafletMapPropsInterface) {
+        super(props);
+        this.iconOptions = {
+            iconUrl: "images/marker-icon.png"
+        };
+        this.icon = new Icon<IconOptions>(this.iconOptions);
+        this.state = {
+            position: [51.505, -0.09]
+        };
+        this.zoom = 10;
+    }
+
+    /**
+     * @param event
+     */
+    handleZoom = (event: LeafletEvent): void => {
+        console.log(event.target._zoom);
+        this.zoom = event.target._zoom;
+    }
+
+    /**
+     * @param event
+     */
+    handleMapClick = (event: LeafletMouseEvent) => {
+        let latlng: LatLng = event.latlng;
+        this.setState({
+            position: [latlng.lat, latlng.lng]
+        })
+    }
+
     render() {
-        const position = [51.505, -0.09] as LatLngTuple;
-        const iconOptions = {iconUrl: "images/marker-icon.png"} as IconOptions;
-        const icon: Icon = new Icon<IconOptions>(iconOptions);
         return (
-            <Map center={position} zoom={13}>
+            <Map center={this.state.position} zoom={this.zoom} onclick={this.handleMapClick} onzoomend={this.handleZoom}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={position} icon={icon}>
+                <Marker position={this.state.position} icon={this.icon}>
                     <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
                 </Marker>
             </Map>
