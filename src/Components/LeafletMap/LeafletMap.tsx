92fc0e527248a0730199bdd51cce73217a1bdca8
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-11-29 10:44:18 +0100

    Map switch added.

--- src/Components/LeafletMap/LeafletMap.tsx
+++ src/Components/LeafletMap/LeafletMap.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import {Icon, IconOptions, LatLng, LatLngTuple, LeafletEvent, LeafletMouseEvent} from "leaflet";
 import './LeafletMap.css';
+import Switch from "../Switch/Switch";
+import PositionInterface from "../../Client/Interface/Data/PositionInterface";
 
 export interface LeafletMapPropsInterface {
     updateParent: (newValues: object) => void
@@ -11,11 +13,17 @@ export interface LeafletMapStateInterface {
     position: LatLngTuple
 }
 
+export interface LeafletMapValues {
+    position?: PositionInterface
+}
+
 export default class LeafletMap extends React.Component<LeafletMapPropsInterface, LeafletMapStateInterface> {
 
     iconOptions: IconOptions;
     icon: Icon;
+    active: boolean;
     zoom: number;
+    values: LeafletMapValues;
 
     /**
      * @param props
@@ -29,7 +37,25 @@ export default class LeafletMap extends React.Component<LeafletMapPropsInterface
         this.state = {
             position: [51.505, -0.09]
         };
+        this.active = true;
         this.zoom = 10;
+        this.values = {
+            position: undefined
+        }
+    }
+
+    /**
+     * @param on
+     */
+    switch = (on: boolean) => {
+        this.active = on;
+        if (this.active) {
+            this.props.updateParent(this.values);
+        } else {
+            this.props.updateParent({
+                'position': undefined
+            });
+        }
     }
 
     /**
@@ -45,12 +71,15 @@ export default class LeafletMap extends React.Component<LeafletMapPropsInterface
      */
     handleMapClick = (event: LeafletMouseEvent) => {
         let latlng: LatLng = event.latlng;
-        this.props.updateParent({
-            'position': {
-                'latitude': latlng.lat,
-                'longitude': latlng.lng
+        this.values = {
+            position: {
+                latitude: latlng.lat,
+                longitude: latlng.lng
             }
-        });
+        };
+        if (this.active) {
+            this.props.updateParent(this.values);
+        }
         this.setState({
             position: [latlng.lat, latlng.lng]
         });
@@ -58,15 +87,22 @@ export default class LeafletMap extends React.Component<LeafletMapPropsInterface
 
     render() {
         return (
-            <Map center={this.state.position} zoom={this.zoom} onclick={this.handleMapClick} onzoomend={this.handleZoom}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={this.state.position} icon={this.icon}>
-                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-                </Marker>
-            </Map>
+            <React.Fragment>
+                <Map center={this.state.position} zoom={this.zoom} onclick={this.handleMapClick} onzoomend={this.handleZoom}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={this.state.position} icon={this.icon}>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    </Marker>
+                </Map>
+                <div className={"d-flex flex-end"}>
+                    <Switch
+                        switchParent={this.switch}
+                    />
+                </div>
+            </React.Fragment>
         );
     }
 
